361936a105cfe043439b8805075d44cd14232ab4
narwadenilesh <nileshnarwade16@gmail.com>
2026-06-28 00:24:25 +0530

    implemented props and tailwind functioality

---
+++ 04_tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+  return (
+    <>  
+      <Card username = "Nilesh"/> 
+      <Card someObject = {{"name" : "Nilesh"}}/> 
+      <Card/> 
+    </>
+  )
+}
+
+export default App
